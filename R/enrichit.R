b7458c14cca5aa97bea30b8eb45561d1aac8cf07
pwwang <pwwang@pwwang.com>
2025-04-22 20:47:36 -0700

    init

---
+++ R/enrichit.R
@@ -0,0 +1,247 @@
+#' Build the numbers for the tests
+#'
+#' @param userlist Vector of user-provided genes
+#' @param geneset Vector of genes in the gene set
+#' @param background Vector of all genes in the universe or a number of genes
+#' in the universe
+#' @return A list with the following elements:
+#' * k: Number of genes in the user list that are also in the gene set
+#' * M: Number of genes in the gene set
+#' * n: Number of genes in the user list
+#' * N: Total number of genes in the universe
+#' @keywords internal
+BuildNumbers <- function(userlist, geneset, background) {
+  k <- length(intersect(userlist, geneset))
+  M <- length(geneset)
+  n <- length(userlist)
+  N <- if (is.numeric(background)) background else length(background)
+  return(list(k = k, M = M, n = n, N = N))
+}
+
+
+#' Compute statistic p-value by given numbers
+#'
+#' @param k Number of genes in both user list and gene set
+#' @param M Number of genes in the gene set
+#' @param n Number of genes in the user list
+#' @param N Total number of genes in the universe
+#' @param method Method for computing p-value, either "fisher" or "hypergeometric"
+#' @return p-value from the specified test
+#' @keywords internal
+#' @importFrom stats fisher.test phyper
+ComputePValue <- function(k, M, n, N, method = c("fisher", "hypergeometric")) {
+    method <- match.arg(method)
+    if (method == "fisher") {
+        mat <- matrix(c(k, n - k, M - k, N - M - n + k), nrow = 2)
+        return(fisher.test(mat, alternative = "greater")$p.value)
+    } else {  # method == "hypergeometric")
+        return(phyper(k - 1, M, N - M, n, lower.tail = FALSE))
+    }
+}
+
+
+#' Check user list against gene sets
+#'
+#' @param userlist Vector of user-provided genes
+#' @param genesets List of gene sets
+#' @param use_matched_only Logical, whether to return only matched genes
+#' Otherwise the genes in the user list will be all used.
+#' This will affect the number of genes in the user list when computing the
+#' p-value.
+#' @param show_all_unmatched Logical, whether to show all unmatched genes. Or instead
+#' show only the first 10 unmatched genes.
+#' @return A vector of user-provided genes that are also in the gene sets
+#' @keywords internal
+CheckUserList <- function(userlist, genesets, use_matched_only, show_all_unmatched = FALSE) {
+    gs <- unique(unlist(genesets))
+    if (length(userlist) == 0) {
+        stop("[EnrichIt] User list is empty.")
+    }
+    userlist <- unique(userlist)
+    unmatched <- setdiff(userlist, gs)
+    if (length(unmatched) > 0) {
+        if (isFALSE(show_all_unmatched)) {
+            n_unmatched <- length(unmatched)
+            if (n_unmatched > 10) {
+                unmatched <- unmatched[1:10]
+                warning("User list contains ", n_unmatched, " unmatched genes. Showing first 10: ", paste(unmatched, collapse = ", "))
+            } else {
+                warning("User list contains ", n_unmatched, " unmatched genes: ", paste(unmatched, collapse = ", "))
+            }
+        } else {
+            warning("User list contains unmatched genes: ", paste(unmatched, collapse = ", "))
+        }
+    }
+    if (isTRUE(use_matched_only)) {
+        userlist <- intersect(userlist, gs)
+    }
+    if (length(userlist) == 0) {
+        stop("[EnrichIt] User list contains no genes that are in the gene sets.")
+    }
+    return(userlist)
+}
+
+
+#' Perform enrichment analysis
+#'
+#' @param userlist Vector of user-provided genes
+#' @param dbs List of gene sets or paths to GMT files
+#' It can be a vector of database names. You can set the names of the vector, which
+#' will be used as the database names.
+#' Otherwise a list is expected, where each element is a database (e.g. parsed from a gmt file).
+#' If a database is given directly (without a name), the expression of this argument will
+#' be used as the database name.
+#' @param method Method for computing p-value, either "fisher" or "hypergeometric"
+#' When `style` is "enrichr", the method defaults to "fisher".
+#' When `style` is "clusterProfiler", the method defaults to "hypergeometric".
+#' @param use_matched_only Logical, whether to use only matched genes against the gene sets.
+#' This will affect the number of genes in the user list when computing the
+#' p-value.
+#' By default, when `style` is "enrichr", this is set to FALSE.
+#' When `style` is "clusterProfiler", this is set to TRUE.
+#' @param padjust_method Method for adjusting p-values, either "BH", "bonferroni",
+#' "holm", "hochberg", "hommel", "BY", "fdr"
+#' @param background Vector of all genes in the universe or a number of genes
+#' in the universe. If NULL, the number of genes in the gene set will be used.
+#' For "enrichr", the default is 20,000.
+#' For "clusterProfiler", the default is the number of unique genes in the gene set.
+#' Note that for "enrichr", if a vector is given, the length of it will be used, no
+#' checking will be done to see if userlist and genes from dbs are in the vector.
+#' @param style Style of the output, either "enrichr" or "clusterProfiler"
+#' @param return_all Logical, whether to return all results (all gene sets in dbs)
+#' or only those with at least one gene in the user list.
+#' @returns A data frame with the results.
+#' When `style` is "enrichr", the columns are:
+#' * Database: Name of the database
+#' * Term: Name of the term
+#' * Overlap: Number of genes in the user list and the gene set
+#' * P.value: p-value from the enrichment test
+#' * Adjusted.P.value: Adjusted p-value from the enrichment test
+#' * Odds.Ratio: Odds ratio from the enrichment test
+#' * Combined.Score: Combined score from the enrichment test
+#' * Genes: Genes in the user list that are also in the gene set
+#' * Rank: Rank of the term based on the combined score
+#' When `style` is "clusterProfiler", the columns are:
+#' * ID: Name of the term
+#' * Description: Description of the term
+#' * GeneRatio: Ratio of genes in the user list and the gene set
+#' * BgRatio: Ratio of genes in the gene set and the universe
+#' * Count: Number of genes in the user list that are also in the gene set
+#' * pvalue: p-value from the enrichment test
+#' * p.adjust: Adjusted p-value from the enrichment test
+#' * qvalue: Q-value from the enrichment test
+#' * geneID: Genes in the user list that are also in the gene set
+#' * Database: Name of the database
+#' @export
+#' @importFrom rlang %||%
+#' @importFrom stats p.adjust
+#' @importFrom tools file_path_sans_ext
+#' @importFrom qvalue qvalue
+EnrichIt <- function(
+    userlist, dbs,
+    method = ifelse(tolower(style[1]) == "enrichr", "fisher", "hypergeometric"),
+    use_matched_only = ifelse(tolower(style[1]) == "enrichr", FALSE, TRUE),
+    padjust_method = c("BH", "bonferroni", "holm", "hochberg", "hommel", "BY", "fdr"),
+    background = NULL,
+    style = c("enrichr", "Enrichr", "clusterProfiler", "ClusterProfiler", "clusterprofiler"),
+    return_all = FALSE
+) {
+    method <- match.arg(method, c("fisher", "hypergeometric"))
+    padjust_method <- match.arg(padjust_method)
+    style <- match.arg(style)
+    style <- tolower(style)
+    dbs_name <- deparse(substitute(dbs))
+
+    if (is.character((dbs))) {
+        if (is.null(names(dbs))) {
+            names(dbs) <- file_path_sans_ext(gsub("\\.gz$", "", basename(dbs)))
+        }
+        dbs <- lapply(dbs, FetchGMT)
+        dbs <- lapply(dbs, ParseGMT)
+    } else if (!is.list(dbs)) {
+        stop("[EnrichIt] Invalid input for dbs.")
+    } else {
+        if (all(sapply(dbs, is.character))) {
+            dbs <- list(dbs)
+            names(dbs) <- dbs_name
+        }
+    }
+
+    results <- lapply(names(dbs), function(db_name) {
+        db <- dbs[[db_name]]
+        db_gene_count <- length(unique(unlist(db)))
+        background <- background %||% ifelse(style == "enrichr", max(20000, db_gene_count), db_gene_count)
+        background <- 10922
+        ul <- CheckUserList(userlist, db, use_matched_only = use_matched_only)
+        res <- lapply(seq_along(db), function(i) {
+            term_name <- names(db)[i]
+            genes <- db[[term_name]]
+            numbers <- BuildNumbers(ul, genes, background)
+            pvalue <- ComputePValue(numbers$k, numbers$M, numbers$n, numbers$N, method)
+            if (style == "enrichr") {
+                # oddsRatio = (1.0 * a * d) / Math.max(1.0 * b * c, 1)
+                #  where: a are the overlapping genes,
+                #  b are the genes in the annotated set - overlapping genes,
+                #  c are the genes in the input set - overlapping genes, and
+                #  d are the 20,000 genes (or total genes in the background) - genes in the annotated set - genes in the input set + overlapping genes
+                a <- numbers$k
+                b <- numbers$M - numbers$k
+                c <- numbers$n - numbers$k
+                d <- numbers$N - numbers$M - numbers$n + numbers$k
+                odds_ratio <- (a * d) / max(1, b * c)
+                return(data.frame(
+                    Database = db_name,
+                    Term = term_name,
+                    Overlap = paste(numbers$k, numbers$M, sep = "/"),
+                    P.value = pvalue,
+                    Odds.Ratio = odds_ratio,
+                    Combined.Score = -log(pvalue) * odds_ratio,
+                    Genes = paste(intersect(genes, ul), collapse = ";"),
+                    stringsAsFactors = FALSE
+                ))
+            } else {
+                # For other output styles, you can modify this part
+                return(data.frame(
+                    Database = db_name,
+                    ID = paste0(db_name, "_", i),
+                    Description = term_name,
+                    GeneRatio = paste(numbers$k, numbers$n, sep = "/"),
+                    BgRatio = paste(numbers$M, numbers$N, sep = "/"),
+                    Count = numbers$k,
+                    pvalue = pvalue,
+                    geneID = paste(intersect(genes, ul), collapse = "/"),
+                    stringsAsFactors = FALSE
+                ))
+            }
+        })
+        res <- do.call(rbind, res)
+        if (style == "enrichr") {
+            if (isFALSE(return_all)) {
+                res <- res[res$Odds.Ratio > 0, , drop = FALSE]
+            }
+            res$k <- NULL
+            res$Adjusted.P.value <- p.adjust(res$P.value, method = padjust_method)
+            res$Rank <- rank(res$P.value, ties.method = "min")
+            res <- res[
+                order(res$Rank),
+                c("Term", "Overlap", "P.value", "Adjusted.P.value", "Odds.Ratio", "Combined.Score", "Genes", "Rank", "Database"),
+                drop = FALSE
+            ]
+        } else {  # style == "clusterprofiler"
+            if (isFALSE(return_all)) {
+                res <- res[res$Count > 0, , drop = FALSE]
+            }
+            res$p.adjust <- p.adjust(res$pvalue, method = padjust_method)
+            res$qvalue <- tryCatch(qvalue(p=res$pvalue, lambda=0.05, pi0.method="bootstrap")$qvalues, error = function(e) NA)
+            res <- res[
+                order(res$pvalue),
+                c("ID", "Description", "GeneRatio", "BgRatio", "pvalue", "p.adjust", "qvalue", "geneID", "Count", "Database"),
+                drop = FALSE
+            ]
+        }
+
+        res
+    })
+
+    return(do.call(rbind, results))
+}
---
+++ R/gmt.R
@@ -0,0 +1,172 @@
+#' Fetch GMT file
+#'
+#' @param gmtpath Path to the GMT file
+#' Either a local file path or a URL.
+#' If a URL is provided, the file will be downloaded and cached.
+#' If a local file path is provided, it will be checked for existence.
+#' It can also be a name of a library from the Enrichr database.
+#' See https://maayanlab.cloud/Enrichr/#libraries.
+#' @return If `gmtpath` is a local file, it returns the path.
+#' If `gmtpath` is a URL, it downloads the file to a temporary cache
+#' directory if not already present and returns the path to the cached file.
+#' @importFrom digest digest
+#' @importFrom utils download.file
+#' @importFrom httr parse_url
+#' @importFrom rlang %||%
+#' @importFrom tools file_path_sans_ext
+#' @keywords internal
+#' @examples
+#' \donttest{
+#' if (FALSE) {
+#' # Fetch a GMT file from a URL (will be cached)
+#' gmtpath_url <- paste0(
+#'   "https://maayanlab.cloud/Enrichr/geneSetLibrary?mode=text&libraryName=",
+#'   "Data_Acquisition_Method_Most_Popular_Genes"
+#' )
+#' FetchGMT(gmtpath_url)
+#' # Example output: ~/.cache/enrichit/xxxxxx.gmt
+#'
+#' FetchGMT("Data_Acquisition_Method_Most_Popular_Genes")
+#' # Example output: ~/.cache/enrichit/xxxxxx.gmt
+#'
+#' # Fetching the same URL again uses the cache
+#' gmt_file_url_cached <- FetchGMT(gmtpath_url)
+#' # Should be the same path as gmt_file_url
+#' print(identical(gmt_file_url, gmt_file_url_cached)) # TRUE
+#'
+#' # If the GMT file is local
+#' # Create a dummy local file for example
+#' local_gmt_path <- tempfile(fileext = ".gmt")
+#' writeLines(c("GENESET1\tdesc1\tGENE1\tGENE2", "GENESET2\tdesc2\tGENE3\tGENE4"), local_gmt_path)
+#' FetchGMT(local_gmt_path)
+#' # Example output: /tmp/RtmpXXXXXX/filexxxx.gmt (original path)
+#' }
+#' }
+FetchGMT <- function(gmtpath) {
+    if (gmtpath %in% c("BioCarta", "BioCarta_2016")) {
+        gmtpath <- system.file("extdata", "BioCarta_2016.gmt.gz", package = "enrichit")
+    } else if (gmtpath %in% c("GO_Biological_Process", "GO_Biological_Process_2025")) {
+        gmtpath <- system.file("extdata", "GO_Biological_Process_2025.gmt.gz", package = "enrichit")
+    } else if (gmtpath %in% c("GO_Cellular_Component", "GO_Cellular_Component_2025")) {
+        gmtpath <- system.file("extdata", "GO_Cellular_Component_2025.gmt.gz", package = "enrichit")
+    } else if (gmtpath %in% c("GO_Molecular_Function", "GO_Molecular_Function_2025")) {
+        gmtpath <- system.file("extdata", "GO_Molecular_Function_2025.gmt.gz", package = "enrichit")
+    } else if (gmtpath %in% c("KEGG", "KEGG_Human", "KEGG_2021", "KEGG_2021_Human")) {
+        gmtpath <- system.file("extdata", "KEGG_2021_Human.gmt.gz", package = "enrichit")
+    } else if (gmtpath %in% c("Hallmark", "MSigDB_Hallmark", "MSigDB_Hallmark_2020")) {
+        gmtpath <- system.file("extdata", "MSigDB_Hallmark_2020.gmt.gz", package = "enrichit")
+    } else if (gmtpath %in% c("Reactome", "Reactome_Pathways", "Reactome_Pathways_2024")) {
+        gmtpath <- system.file("extdata", "Reactome_Pathways_2024.gmt.gz", package = "enrichit")
+    } else if (gmtpath %in% c("WikiPathways", "WikiPathways_2024", "WikiPathways_Human", "WikiPathways_2024_Human")) {
+        gmtpath <- system.file("extdata", "WikiPathways_2024_Human.gmt.gz", package = "enrichit")
+    } else if (grepl("^[a-zA-Z0-9_-]+$", gmtpath)) {
+        # Check if the GMT file is available in Enrichr
+        gmtpath <- paste0("https://maayanlab.cloud/Enrichr/geneSetLibrary?mode=text&libraryName=", gmtpath)
+    }
+    # Check if the path is a URL
+    if (grepl("^https?://", gmtpath)) {
+        # Use a persistent user cache directory
+        cache_dir <- tools::R_user_dir("enrichit", which = "cache")
+        # Generate a unique filename based on the URL hash
+        file_hash <- substr(digest(gmtpath, algo = "md5"), 1, 8)
+        cache_dir <- file.path(cache_dir, file_hash)
+        if (!dir.exists(cache_dir)) {
+            dir.create(cache_dir, recursive = TRUE, showWarnings = FALSE)
+        }
+        # Parse the URL to get the libraryName
+        parsed_url <- parse_url(gmtpath)
+        library_name <- parsed_url$query$libraryName %||% file_path_sans_ext(sub("\\.gz$", "", basename(gmtpath)))
+
+        # Create the target file path
+        target_file <- file.path(cache_dir, paste0(library_name, ".gmt"))
+
+        # Check if the file exists in cache and is not empty
+        if (file.exists(target_file) && file.info(target_file)$size > 0) {
+            message("Using cached file: ", target_file)
+            return(target_file)
+        } else {
+            message("Downloading GMT file from URL: ", gmtpath)
+            # Download the file
+            tryCatch({
+                download_status <- download.file(gmtpath, target_file, mode = "wb", quiet = TRUE)
+                if (download_status != 0) {
+                   stop("[EnrichIt] Failed to download file. Status code: ", download_status)
+                }
+                # Verify download integrity (basic check: file exists and is not empty)
+                if (!file.exists(target_file) || file.info(target_file)$size == 0) {
+                    stop("Downloaded file is empty or does not exist: ", target_file)
+                }
+                message("File downloaded and cached successfully: ", target_file)
+                return(target_file)
+            }, error = function(e) {
+                # Clean up potentially incomplete file on error
+                if (file.exists(target_file)) {
+                    unlink(target_file)
+                }
+                stop("[EnrichIt] Error downloading ", gmtpath, ": ", e$message)
+            })
+        }
+    } else {
+        # If it's a local file, check existence and return the path
+        if (file.exists(gmtpath)) {
+            return(gmtpath)
+        } else {
+            stop("[EnrichIt] The specified local GMT file does not exist: ", gmtpath)
+        }
+    }
+}
+
+
+#' Read GMT file
+#'
+#' @param gmtfile Path to the GMT file
+#' @param swap_name_desc_if_needed Logical, whether to swap name and description fields
+#' if all descriptions are shorter than names.
+#' @return A list of gene sets, the names of the list are the gene set names,
+#' and each element is a vector of gene names.
+#' @export
+#' @examples
+#' \donttest{
+#' if (FALSE) {
+#' # Example GMT file content
+#' gmtfile <- system.file("extdata", "KEGG_2021_Human.gmt", package = "enrichit")
+#' # Read the GMT file
+#' ParseGMT(gmtfile)
+#' }
+#' }
+ParseGMT <- function(gmtfile, swap_name_desc_if_needed = TRUE) {
+    gmtfile <- FetchGMT(gmtfile)
+    # Support gzipped files
+    con <- if (grepl("\\.gz$", gmtfile)) gzfile(gmtfile) else gmtfile
+    on.exit(if (inherits(con, "connection")) close(con), add = TRUE)
+    lines <- readLines(con, warn = FALSE)
+    # Parse lines into gene sets
+    libraries <- lapply(lines, function(line) {
+        fields <- strsplit(line, "\t", fixed = TRUE)[[1]]
+        list(name = fields[1], desc = fields[2], genes = fields[-(1:2)])
+    })
+    # Remove empty gene sets and empty genes
+    libraries <- Filter(function(lib) length(lib$genes) > 0, libraries)
+    libraries <- lapply(libraries, function(lib) {
+        lib$genes <- lib$genes[nzchar(lib$genes)]
+        lib
+    })
+    # Swap name and desc if needed
+    name_nchars <- vapply(libraries, function(lib) nchar(lib$name), integer(1))
+    desc_nchars <- vapply(libraries, function(lib) nchar(lib$desc), integer(1))
+    if (swap_name_desc_if_needed && all(desc_nchars > 0) && all(desc_nchars < name_nchars)) {
+        warning("Swapping name and desc fields in GMT file, as desc is shorter. Set 'swap_name_desc_if_needed' to FALSE to disable this.")
+        libraries <- lapply(libraries, function(lib) {
+            tmp <- lib$name
+            lib$name <- lib$desc
+            lib$desc <- tmp
+            lib
+        })
+    }
+    # Remove duplicates by name
+    unique_names <- !duplicated(vapply(libraries, `[[`, character(1), "name"))
+    libraries <- libraries[unique_names]
+    # Set names and remove name/desc field
+    names(libraries) <- vapply(libraries, `[[`, character(1), "name")
+    lapply(libraries, `[[`, "genes")
+}
